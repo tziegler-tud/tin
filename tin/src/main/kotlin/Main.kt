7d8e8adbc2b8bbd7d9c227478e38a11c6742fad0
Robin Balzer <robin.balzer@mailbox.tu-dresden.de>
2023-03-13 12:41:36 +0100

    comment out debugging prints

--- tin/src/main/kotlin/Main.kt
+++ tin/src/main/kotlin/Main.kt
@@ -6,8 +6,9 @@ fun main() {
     val inputReaderService = InputReaderService()
     val fileInputData = FileInputData("tin/src/main/resources/exampleQueryGraph.txt", "tin/src/main/resources/exampleTransducerGraph.txt", "tin/src/main/resources/exampleDatabaseGraph.txt", GenerateTransducer.classicAnswers)
     val dataProvider = inputReaderService.importRegularPathQueryFromSeparateFiles(fileInputData)
-    println("query node size: " + dataProvider.queryGraph.nodes.size)
-    println("transducer node size: " + dataProvider.transducerGraph.nodes.size)
-    println("database node size: " + dataProvider.databaseGraph.nodes.size)
+
+    // println("query node size: " + dataProvider.queryGraph.nodes.size)
+    // println("transducer node size: " + dataProvider.transducerGraph.nodes.size)
+    // println("database node size: " + dataProvider.databaseGraph.nodes.size)
 
 }
