f439060111ff167d687051d3ab8d8fb41885d9d4
Robin Balzer <robin.balzer@mailbox.tu-dresden.de>
2023-03-25 20:14:41 +0100

    fix gradle build stuff

--- tin/src/main/kotlin/Main.kt
+++ tin/src/main/kotlin/Main.kt
@@ -2,13 +2,17 @@ import tin.data.input.FileInputData
 import tin.data.input.GenerateTransducer
 import tinput.services.InputReaderService
 
-fun main() {
-    val inputReaderService = InputReaderService()
-    val fileInputData = FileInputData("tin/src/main/resources/exampleQueryGraph.txt", "tin/src/main/resources/exampleTransducerGraph.txt", "tin/src/main/resources/exampleDatabaseGraph.txt", GenerateTransducer.classicAnswers)
-    val dataProvider = inputReaderService.importRegularPathQueryFromSeparateFiles(fileInputData)
+class Main {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val inputReaderService = InputReaderService()
+            val fileInputData = FileInputData("tin/src/main/resources/exampleQueryGraph.txt", "tin/src/main/resources/exampleTransducerGraph.txt", "tin/src/main/resources/exampleDatabaseGraph.txt", GenerateTransducer.classicAnswers)
+            val dataProvider = inputReaderService.importRegularPathQueryFromSeparateFiles(fileInputData)
 
-    // println("query node size: " + dataProvider.queryGraph.nodes.size)
-    // println("transducer node size: " + dataProvider.transducerGraph.nodes.size)
-    // println("database node size: " + dataProvider.databaseGraph.nodes.size)
-
+            // println("query node size: " + dataProvider.queryGraph.nodes.size)
+            // println("transducer node size: " + dataProvider.transducerGraph.nodes.size)
+            // println("database node size: " + dataProvider.databaseGraph.nodes.size)
+        }
+    }
 }
