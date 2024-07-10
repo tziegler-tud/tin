4a7908c6a34d215a205d428b9a07b10c7c312766
Tom Ziegler <tom.ziegler1@gmx.de>
2024-07-10 17:05:08 +0200

    WIP: Working on ontology integration

--- src/main/kotlin/tin/services/ontology/OntologyManager.kt
+++ src/main/kotlin/tin/services/ontology/OntologyManager.kt
@@ -1,5 +1,42 @@
 package tin.services.ontology
 
-class OntologyManager {
+import org.semanticweb.elk.owlapi.ElkReasonerFactory
+import org.semanticweb.owlapi.apibinding.OWLManager
+import org.semanticweb.owlapi.model.OWLAxiom
+import org.semanticweb.owlapi.model.OWLOntology
+import org.semanticweb.owlapi.reasoner.InferenceType
+import org.semanticweb.owlapi.reasoner.OWLReasoner
+import org.semanticweb.owlapi.reasoner.OWLReasonerFactory
+import org.semanticweb.owlapi.util.InferredAxiomGenerator
+import org.semanticweb.owlapi.util.InferredEquivalentClassAxiomGenerator
+import org.semanticweb.owlapi.util.InferredSubClassAxiomGenerator
+import java.io.File
 
+
+class OntologyManager() {
+
+    private val manager = OWLManager.createOWLOntologyManager();
+    private lateinit var ontology: OWLOntology;
+    private lateinit var reasoner: OWLReasoner;
+
+    fun loadOntology(file: File){
+        val ontology = manager.loadOntologyFromOntologyDocument(file)
+
+    }
+
+    fun loadElkReasoner(ontology: OWLOntology){
+        // We need a reasoner to do our query answering
+        val reasonerFactory: OWLReasonerFactory = ElkReasonerFactory()
+        val reasoner = reasonerFactory.createReasoner(ontology)
+
+        // Classify the ontology.
+        reasoner.precomputeInferences(InferenceType.CLASS_HIERARCHY)
+
+        // To generate an inferred ontology we use implementations of
+        // inferred axiom generators
+//        val gens: MutableList<InferredAxiomGenerator<out OWLAxiom?>> = ArrayList()
+//        gens.add(InferredSubClassAxiomGenerator())
+//        gens.add(InferredEquivalentClassAxiomGenerator())
+        reasoner.dispose();
+    }
 }
