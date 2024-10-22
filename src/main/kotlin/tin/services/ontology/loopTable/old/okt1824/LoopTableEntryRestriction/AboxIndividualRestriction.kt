e26a07712870619a9b4b0df16f3b341bb2dc67f0
Tom Ziegler <tom.ziegler1@gmx.de>
2024-10-22 16:38:36 +0200

    removed archive

--- src/main/kotlin/tin/services/ontology/loopTable/old/okt1824/LoopTableEntryRestriction/AboxIndividualRestriction.kt
+++
@@ -1,6 +0,0 @@
-package tin.services.ontology.loopTable.old.okt1824.LoopTableEntryRestriction//package tin.services.ontology.loopTable.LoopTableEntryRestriction
-//
-//import tin.model.v1.graph.Node
-//
-//class AboxIndividualRestriction(override val value: Node) : LoopTableEntryRestriction<Any> {
-//}
