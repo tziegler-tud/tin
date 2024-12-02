601b972ccf7d4c1589550ae8bffe20d9f4d71cda
Tom Ziegler <tom.ziegler1@gmx.de>
2024-12-02 17:15:50 +0100

    added utils to numeric ec + restrictions

--- src/main/kotlin/tin/services/ontology/loopTable/loopTableEntry/ELHI/SPANumericLoopTableEntry.kt
+++ src/main/kotlin/tin/services/ontology/loopTable/loopTableEntry/ELHI/SPANumericLoopTableEntry.kt
@@ -1,6 +1,7 @@
 package tin.services.ontology.loopTable.loopTableEntry.ELHI
 
 import tin.model.v2.graph.Node
+import tin.services.ontology.loopTable.LoopTableEntryRestriction.spa.MultiClassLoopTableEntryRestriction
 import tin.services.ontology.loopTable.LoopTableEntryRestriction.spa.NumericConceptNameRestriction
 
 class SPANumericLoopTableEntry(
@@ -8,4 +9,6 @@ class SPANumericLoopTableEntry(
     override val target: Pair<Node, Node>,
     override val restriction: NumericConceptNameRestriction,
     ) : ELHISPALoopTableEntry(source, target, restriction) {
+    constructor(querySource: Node, transducerSource: Node, queryTarget: Node, transducerTarget: Node, restriction: NumericConceptNameRestriction)
+            : this(Pair(querySource, transducerSource), Pair(queryTarget, transducerTarget), restriction)
 }
