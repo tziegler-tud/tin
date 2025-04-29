32b3f7bf1108bc44c9954abcd8bc34dd671d8cdf
Tom Ziegler <tom.ziegler1@gmx.de>
2025-04-29 14:40:53 +0200

    renamed module to tinDB (continued)

---
+++ tinDB/src/main/kotlin/tinDB/services/internal/dijkstra/DijkstraQueryAnsweringUtils.kt
@@ -0,0 +1,26 @@
+package tinDB.services.internal.dijkstra
+
+import org.springframework.stereotype.Service
+import tinDB.model.v1.queryResult.RegularPathQueryResult
+import tinDB.model.v1.utils.ProductAutomatonTuple
+
+@Service
+class DijkstraQueryAnsweringUtils {
+
+    /**
+     * transforms internal answerMap containing a (source, target) ProductAutomatonTuple as key, and a Double as value (cost of reaching target from source)
+     * into a set of AnswerTriplets (source.name, target.name, double); omitting the technical ProductAutomatonNodes
+     * after finishing the query we do not care about technical details, we simply want the (human-readable) results.
+     */
+    fun makeAnswerMapReadable(
+        answerMap: HashMap<ProductAutomatonTuple, Double>
+    ): Set<RegularPathQueryResult.AnswerTriplet> {
+        return HashSet<RegularPathQueryResult.AnswerTriplet>().apply {
+            answerMap.forEach { (key, value) ->
+                val source = key.sourceProductAutomatonNode!!.identifier.third.identifier
+                val target = key.targetProductAutomatonNode.identifier.third.identifier
+                add(RegularPathQueryResult.AnswerTriplet(source, target, value))
+            }
+        }
+    }
+}
---
+++ tinDB/src/main/kotlin/tinDB/services/internal/fileReaders/FileReaderWarning.kt
@@ -0,0 +1,7 @@
+package tinDB.services.internal.fileReaders
+
+class FileReaderWarning(
+        var message: String,
+        var index: Int,
+        var line: String,
+)
