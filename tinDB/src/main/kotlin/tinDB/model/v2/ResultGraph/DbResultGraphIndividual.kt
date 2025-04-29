ccfb9d31853fb9cb23454a677b14d6dd227aaf7a
Tom Ziegler <tom.ziegler1@gmx.de>
2025-04-29 14:34:37 +0200

    added data models for DbResultGraph

---
+++ tinDB/src/main/kotlin/tinDB/model/v2/ResultGraph/DbResultGraphIndividual.kt
@@ -0,0 +1,5 @@
+package tinDB.model.v2.ResultGraph
+
+import tinLIB.model.v2.ResultGraph.ResultGraphIndividual
+
+class DbResultGraphIndividual(identifier: String): ResultGraphIndividual(identifier)
